577cbd7cd53801737251d6744d3f1a634d38266b
nathanrun1 <nathanmreilly@gmail.com>
2025-09-28 17:53:27 -0700

    Initial creation of furnace

--- Assets/Scripts/Systems/Obstacles/Furnace.cs
+++ Assets/Scripts/Systems/Obstacles/Furnace.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 using UnityEngine.Assertions;
 using UnityEngine.Rendering.Universal;
 using Sirenix.OdinInspector;
+using Blizzard.Inventory;
 
 namespace Blizzard.Obstacles.Concrete
 {
@@ -9,9 +10,14 @@ namespace Blizzard.Obstacles.Concrete
     {
         public string PrimaryInteractText { get; private set; } = "Smelt";
 
+
+        private InventorySlot _ingredient;
+        private InventorySlot _fuel;
+        private InventorySlot _result;
+
         public void OnPrimaryInteract()
         {
-            Debug.Log("FURNACE INTERACTION!");
+            
         }
     }
 }
