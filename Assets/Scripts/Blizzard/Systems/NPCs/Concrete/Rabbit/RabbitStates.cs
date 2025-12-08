47ae91258349c51fb624a75c7afb6ee0ef727662
nathanrun1 <nathanmreilly@gmail.com>
2025-12-07 16:19:41 -0800

    Switched to bruteforce rabbit pathfinding, rabbit logic working

--- Assets/Scripts/Blizzard/Systems/NPCs/Concrete/Rabbit/RabbitStates.cs
+++ Assets/Scripts/Blizzard/Systems/NPCs/Concrete/Rabbit/RabbitStates.cs
@@ -16,13 +16,27 @@ namespace Blizzard.NPCs.Concrete.Rabbit
     public class RabbitConfig
     {
         public float walkSpeed;
-
+        
+        /// <summary>
+        /// Range at which rabbit starts to flee from player
+        /// </summary>
         public float playerEnterFleeRange;
+        /// <summary>
+        /// Range at which rabbit stops fleeing from player
+        /// </summary>
         public float playerExitFleeRange;
+        /// <summary>
+        /// Range of time intervals between entering idle state and starting to wander
+        /// </summary>
         public Vector2 wanderPeriodRange;
+        /// <summary>
+        /// Range of time that a rabbit wanders for
+        /// </summary>
         public Vector2 wanderTimeRange;
-
-        public float idleUpdateDelay;
+        /// <summary>
+        /// Degrees/second that the rabbit's travel direction can rotate toward its intended travel direction
+        /// </summary>
+        public float dirAdjustmentRate;
     }
     
     public class RabbitContext : IStateContext
@@ -40,11 +54,13 @@ namespace Blizzard.NPCs.Concrete.Rabbit
     
     public abstract class RabbitState : IState
     {
-        protected RabbitContext context;
+        protected RabbitContext _ctx;
+
+        private Vector2 _curDirection;
         
         public virtual void Enter(IStateContext ctx)
         {
-            context = ctx as RabbitContext;;
+            _ctx = ctx as RabbitContext;;
         }
 
         public abstract void Update(float deltaTime);
@@ -55,17 +71,49 @@ namespace Blizzard.NPCs.Concrete.Rabbit
 
         
         /// <summary>
-        /// Travels toward the given position
+        /// Move in the current intended direction
         /// </summary>
-        protected void MoveToward(Vector2 position)
+        protected void Move()
         {
-            Vector2 movementVector = (position - (Vector2)context.transform.position).normalized;
-            context.rigidbody.MovePosition(context.rigidbody.position + movementVector.normalized *
-                (context.config.walkSpeed * Time.fixedDeltaTime));
+            _ctx.rigidbody.MovePosition(_ctx.rigidbody.position + _curDirection.normalized *
+                (_ctx.config.walkSpeed * Time.fixedDeltaTime));
         }
         
         /// <summary>
-        /// Picks the next position to travel to based on the desired direction to travel in.
+        /// Attempt to stay in the same position
+        /// </summary>
+        protected void Freeze()
+        {
+            _ctx.rigidbody.MovePosition(_ctx.transform.position);
+        }
+
+        /// <summary>
+        /// Sets current direction to given target direction
+        /// </summary>
+        protected void SetDirectionTo(Vector2 targetDir)
+        {
+            _curDirection = targetDir;
+        }
+        
+        /// <summary>
+        /// Smoothly adjusts (interpolates) the current travel direction toward the intended travel direction.
+        /// </summary>
+        protected void AdjustDirectionToward(Vector2 targetDir, float deltaTime)
+        {
+            BLog.Log($"Intended dir: {targetDir}, actual: {_curDirection}");
+            
+            float signedAngle = Mathf.Atan2(
+                    _curDirection.x * targetDir.y - _curDirection.y * targetDir.x,  // cross product
+                    Vector2.Dot(_curDirection, targetDir)                   // dot product
+            );
+            float rotDir = Mathf.Sign(signedAngle);
+            BLog.Log($"Rotation dir: {rotDir}");
+            Quaternion rot = Quaternion.Euler(0, 0, rotDir * deltaTime * _ctx.config.dirAdjustmentRate);
+            _curDirection = rot * _curDirection;
+        }
+        
+        /// <summary>
+        /// Picks the next direction to travel in to based on the desired direction to travel in.
         ///
         /// Assumes direction is normalized.
         /// </summary>
@@ -76,41 +124,35 @@ namespace Blizzard.NPCs.Concrete.Rabbit
             // Ensure that next travel position is at most one grid square away
             Vector2 curDir = direction *= GameConstants.CellSideLength;
 
-            if (curDir.ToCellPos() == Vector2Int.zero)
+            // We alternate by checking some angle both rotated clockwise & counterclockwise, increasing that angle
+            //   iteratively.
+            int maxDoubleChecks = PathfindingConstants.maxDirectionRotationChecks / 2;
+            for (int i = 0; i < maxDoubleChecks; ++i)
             {
-                // Next position not in adj grid square, degrade to brute force (check a lot of rotations)
-                for (int i = 0; i < PathfindingConstants.maxDirectionRotationChecks; ++i) 
-                {
-                    Quaternion rot = Quaternion.Euler(0, 0, i / 50f * 360f * (int)adjRot);
-                    Vector2 targetPos = (Vector2)context.transform.position + (Vector2)(rot * curDir);
-                    if (!context.obstacleGridService.IsOccupied(targetPos.ToCellPos()))
-                        return targetPos;
-                }
-            }
-            else
-            {
-                for (int i = 0; i < 8; ++i)
-                {
-                    Vector2 targetPos = (Vector2)context.transform.position + curDir;
+                float clockwiseAngle = i / (float)maxDoubleChecks * 180f;
+                float counterclockwiseAngle = (i + 1) / (float)maxDoubleChecks * -180f;
+                Quaternion rotClockwise = Quaternion.Euler(0, 0, clockwiseAngle);
+                Quaternion rotCounterclockwise = Quaternion.Euler(0, 0, counterclockwiseAngle);
                 
-                    if (!context.obstacleGridService.IsOccupied(targetPos.ToCellPos()))
-                        return targetPos;
-
-                    curDir = RotateToNextGridSquare(curDir, adjRot);
+                Vector2 targetPos = (Vector2)_ctx.transform.position + (Vector2)(rotClockwise * curDir);
+                if (!_ctx.obstacleGridService.IsOccupied(targetPos.ToCellPos()))
+                    return rotClockwise * curDir;
                 
-                }    
+                targetPos = (Vector2)_ctx.transform.position + (Vector2)(rotCounterclockwise * curDir);
+                if (!_ctx.obstacleGridService.IsOccupied(targetPos.ToCellPos()))
+                    return rotCounterclockwise * curDir;
             }
 
-            return context.transform.position;  // No available options. Stand still.
+            return _ctx.transform.position;  // No available options. Stand still.
         }
-
+        
         /// <summary>
         /// Determines if the rabbit is within range to starting fleeing from the player
         /// </summary>
         protected bool ShouldStartFlee()
         {
-            return (context.playerService.PlayerPosition - (Vector2)context.transform.position).magnitude <=
-                   context.config.playerEnterFleeRange;
+            return (_ctx.playerService.PlayerPosition - (Vector2)_ctx.transform.position).magnitude <=
+                   _ctx.config.playerEnterFleeRange;
         }
 
         /// <summary>
@@ -118,8 +160,8 @@ namespace Blizzard.NPCs.Concrete.Rabbit
         /// </summary>
         protected bool ShouldStopFlee()
         {
-            return (context.playerService.PlayerPosition - (Vector2)context.transform.position).magnitude >=
-                   context.config.playerExitFleeRange;
+            return (_ctx.playerService.PlayerPosition - (Vector2)_ctx.transform.position).magnitude >=
+                   _ctx.config.playerExitFleeRange;
         }
 
         /// <summary>
@@ -137,7 +179,7 @@ namespace Blizzard.NPCs.Concrete.Rabbit
             
             // Constrain one component to be as close as possible to grid square's border, and then adjust other
             //   to maintain magnitude of 1.
-            Vector2 newPoint = (0.5f * GameConstants.CellSideLength + context.collisionRadius)
+            Vector2 newPoint = (0.5f * GameConstants.CellSideLength + _ctx.collisionRadius)
                 * (Vector2)diff + GridAssistant.CellToWorldPosCenter(curSquare);
             newPoint *= new Vector2(Math.Sign(diff.x), Math.Sign(diff.y));
             if (newPoint.x == 0)
@@ -158,19 +200,25 @@ namespace Blizzard.NPCs.Concrete.Rabbit
         {
             base.Enter(ctx);
 
-            _wanderPeriod = Random.Range(context.config.wanderPeriodRange.x, context.config.wanderPeriodRange.y);
+            _wanderPeriod = Random.Range(_ctx.config.wanderPeriodRange.x, _ctx.config.wanderPeriodRange.y);
+            SetDirectionTo((new Vector2(1, 1)).normalized);
         }
 
         public override void Update(float deltaTime)
         {
             _clock += deltaTime;
-
-            if (ShouldStartFlee())
-                context.StateMachine.ChangeState(new FleeState());
-            if (_clock >= _wanderPeriod)
-                context.StateMachine.ChangeState(new WanderState());
             
-            MoveToward(context.transform.position); // Stand still
+            if (ShouldStartFlee())
+                _ctx.StateMachine.ChangeState(new FleeState());
+            if (_clock >= _wanderPeriod)
+                _ctx.StateMachine.ChangeState(new WanderState());
+            
+            Freeze();
+
+            // BLog.Log("moving...");
+            // Vector2 dir = new Vector2(1, 1);
+            // AdjustDirectionToward(TryTravelInDirection(dir.normalized, AdjRotation.Clockwise), deltaTime);
+            // Move();
         }
     }
 
@@ -188,10 +236,12 @@ namespace Blizzard.NPCs.Concrete.Rabbit
         {
             base.Enter(ctx);
 
-            _wanderTime = Random.Range(context.config.wanderTimeRange.x, context.config.wanderTimeRange.y);
+            _wanderTime = Random.Range(_ctx.config.wanderTimeRange.x, _ctx.config.wanderTimeRange.y);
             float randAngle = Random.Range(0, 2 * Mathf.PI);
             _wanderDirection = Vector2.right * Mathf.Cos(randAngle) + Vector2.up * Mathf.Sin(randAngle);
             _adjRot = Random.Range(0, 2) == 0 ? AdjRotation.Clockwise : AdjRotation.Counterclockwise;
+            
+            SetDirectionTo(_wanderDirection);
         }
 
         public override void Update(float deltaTime)
@@ -199,11 +249,12 @@ namespace Blizzard.NPCs.Concrete.Rabbit
             _clock += deltaTime;
 
             if (ShouldStartFlee()) 
-                context.StateMachine.ChangeState(new FleeState());
+                _ctx.StateMachine.ChangeState(new FleeState());
             if (_clock >= _wanderTime)
-                context.StateMachine.ChangeState(new IdleState());
+                _ctx.StateMachine.ChangeState(new IdleState());
             
-            MoveToward(TryTravelInDirection(_wanderDirection, _adjRot));
+            AdjustDirectionToward(TryTravelInDirection(_wanderDirection, _adjRot), deltaTime);
+            Move();
         }
     }
 
@@ -219,15 +270,19 @@ namespace Blizzard.NPCs.Concrete.Rabbit
             base.Enter(ctx);
 
             _adjRot = Random.Range(0, 2) == 0 ? AdjRotation.Clockwise : AdjRotation.Counterclockwise;
+            
+            Vector2 fleeDirection = (Vector2)_ctx.transform.position - _ctx.playerService.PlayerPosition;
+            SetDirectionTo(fleeDirection);
         }
         
         public override void Update(float deltaTime)
         {
-            Vector2 fleeDirection = (Vector2)context.transform.position - context.playerService.PlayerPosition;
-            if (fleeDirection.magnitude >= context.config.playerExitFleeRange)
-                context.StateMachine.ChangeState(new IdleState());
+            Vector2 fleeDirection = (Vector2)_ctx.transform.position - _ctx.playerService.PlayerPosition;
+            if (fleeDirection.magnitude >= _ctx.config.playerExitFleeRange)
+                _ctx.StateMachine.ChangeState(new IdleState());
             
-            MoveToward(TryTravelInDirection(fleeDirection.normalized, _adjRot));
+            AdjustDirectionToward(TryTravelInDirection(fleeDirection.normalized, _adjRot), deltaTime);
+            Move();
         }
     }
 }
